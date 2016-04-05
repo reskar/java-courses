b09db03975d4e7e193a2c5220bde2b5466719807
sergey <blackskyone@gmail.com>
2016-04-05 20:48:25 +0300

    up

--- Calculate.java
+++ Calculate.java
@@ -2,7 +2,7 @@ public class Calculate {
 	public static void main (String args[]) {
 		
 		System.out.println("Calculate....");
-		System.out.println("it's works.");
+		System.out.println("it's works...");
 		
 	}
 	
